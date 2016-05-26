0d3e2d3bbd808323d2c8d75d343fd45806c3dd8c
Aareet Shermon <aareet@users.noreply.github.com>
2016-05-25 21:46:39 -0700

    Removed un-customized exercises and just left one working exercise

--- FollowUpDoc/Shoulder.swift
+++ FollowUpDoc/Shoulder.swift
@@ -25,32 +25,32 @@ class Shoulder: BodyPart {
     
     func initialiseExercises() -> [Exercise]{
         
-        shoulderPress.descriptive = "text"
-        shoulderPress.eSets = 0
-        shoulderPress.eSetsTobeDone = 5
-        shoulderPress.instructions = "text"
-        shoulderPress.isCompleted = false
-        shoulderPress.name = "Shoulder Press"
-        shoulderPress.targetAreas = "Shoulders"
-        exercises.append(shoulderPress)
-        
-        armCircles.descriptive = "text"
-        armCircles.eSets = 0
-        armCircles.eSetsTobeDone = 3
-        armCircles.instructions = "text"
-        armCircles.isCompleted = false
-        armCircles.name = "Arm Cirlces"
-        armCircles.targetAreas = "Shoulders"
-        exercises.append(armCircles)
-
-        bandPullApart.descriptive = "text"
-        bandPullApart.eSets = 0
-        bandPullApart.eSetsTobeDone = 5
-        bandPullApart.instructions = "text"
-        bandPullApart.isCompleted = false
-        bandPullApart.name = "Band Pull Apart"
-        bandPullApart.targetAreas = "Shoulders"
-        exercises.append(bandPullApart)
+//        shoulderPress.descriptive = "text"
+//        shoulderPress.eSets = 0
+//        shoulderPress.eSetsTobeDone = 5
+//        shoulderPress.instructions = "text"
+//        shoulderPress.isCompleted = false
+//        shoulderPress.name = "Shoulder Press"
+//        shoulderPress.targetAreas = "Shoulders"
+//        exercises.append(shoulderPress)
+//        
+//        armCircles.descriptive = "text"
+//        armCircles.eSets = 0
+//        armCircles.eSetsTobeDone = 3
+//        armCircles.instructions = "text"
+//        armCircles.isCompleted = false
+//        armCircles.name = "Arm Cirlces"
+//        armCircles.targetAreas = "Shoulders"
+//        exercises.append(armCircles)
+//
+//        bandPullApart.descriptive = "text"
+//        bandPullApart.eSets = 0
+//        bandPullApart.eSetsTobeDone = 5
+//        bandPullApart.instructions = "text"
+//        bandPullApart.isCompleted = false
+//        bandPullApart.name = "Band Pull Apart"
+//        bandPullApart.targetAreas = "Shoulders"
+//        exercises.append(bandPullApart)
         
         frontRaise.descriptive = "text"
         frontRaise.eSets = 0
@@ -60,16 +60,16 @@ class Shoulder: BodyPart {
         frontRaise.name = "Front Raise"
         frontRaise.targetAreas = "Shoulders"
         exercises.append(frontRaise)
-        
-        lateralRaise.descriptive = "text"
-        lateralRaise.eSets = 0
-        lateralRaise.eSetsTobeDone = 10
-        lateralRaise.instructions = "1. Raise upper arms to sides until elbows are shoulder height.\n2. Maintain elbows' height above or equal to wrists.\n3. Lower and repeat."
-        lateralRaise.isCompleted = false
-        lateralRaise.name = "Lateral Raise"
-        lateralRaise.targetAreas = "Shoulders"
-        exercises.append(lateralRaise)
-        
+//
+//        lateralRaise.descriptive = "text"
+//        lateralRaise.eSets = 0
+//        lateralRaise.eSetsTobeDone = 10
+//        lateralRaise.instructions = "1. Raise upper arms to sides until elbows are shoulder height.\n2. Maintain elbows' height above or equal to wrists.\n3. Lower and repeat."
+//        lateralRaise.isCompleted = false
+//        lateralRaise.name = "Lateral Raise"
+//        lateralRaise.targetAreas = "Shoulders"
+//        exercises.append(lateralRaise)
+//        
         return exercises
     }
 }
